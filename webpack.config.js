52f89891174dbde8a02d5ef98618553e98bdb7df
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2020-08-07 10:58:18 +0500

    The webpack config error solved!
    I tried using variables like DIST_DIR and SRC_DIR but that didn't help in solving the config error... although it did help in shortening the path definitions.
    Later the REAL PROBLEM was found to be a mistake in the spelling that is using "module.export" istead of using "module.exports".... adding an extra 's' solved the proble...
    HOW CARELESS OF ME :stuck_out_tongue:

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path'); 
 
-module.export = {
-    entry: './src/js/index.js',
+var DIST_DIR = path.resolve(__dirname,'dist');
+var SRC_DIR = path.resolve(__dirname,'src');
+
+// module.export = {
+module.exports = {
+    entry: SRC_DIR+'/js/index.js',
+    // entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
+        path: DIST_DIR+'/js',
+        // path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
     mode: 'development'
