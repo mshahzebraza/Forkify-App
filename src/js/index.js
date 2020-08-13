5219bce1598f2514d4a85509e77abe37ce9c6ae4
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2020-08-13 16:47:45 +0500

    Forkify Start: Real API Calls, API Keys,
    - The Food2Fork Web was down so a new API Server was created by Jonas himself...
    - use of AXIOS npm package instead of FETCH to call APIs ...Axios returns json directly and handles the conversion on its own... and is also better at error handling....
    - Error handling is done using the try-catch function.....

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,23 @@
+import axios from 'axios';
 // Global app controller
+const APIpath = `https://forkify-api.herokuapp.com/api/search`; // The Api built by Jonas
 
-// Default Imports
+async function getResults(query) {
+    
+    try {
+        const res = await axios(`${APIpath}?q=${query}`);
+        const recipes = res.data.recipes;
+        
+        console.log(recipes.forEach(el => {
+            console.log(el.title);      
+            console.log(el);      
+            console.log(``);
+        }) );
+        
+    } catch (error) {
+        alert(error)
+    }
 
-import str from './models/Search';
-console.log(`Imported the string "${str}" from "Search" Model.`);
+}
 
-// Named Imports
-
-// import {add, mul, ID} from "./views/searchView"
-// console.log(`Using imported named modules from searchView : ${add(ID,2)} and ${mul(3,5)}`);
-
-// import {add as ModA, mul as ModB, ID} from "./views/searchView"
-// console.log(`Using imported named modules from searchView : ${ModA(ID,2)} and ${ModB(3,5)}`);
-
-import * as searchView from './views/searchView';
-console.log(`Using imported named modules from searchView : ${searchView.add( searchView.ID, 2 )} and ${ searchView.mul(3,5)}`);
+getResults(`pizza`);
