620c4b71206dae056b596e34eec33afc284d4b18
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2020-08-21 15:20:33 +0500

    140 - Building the Search Model
    - Use of classes to create an object constructor for every search query.
    - the search object is modelled to contain and store the query and a ASYNC method which can be invoked to return and display the results from the API call based on the query.

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,6 @@
-import axios from 'axios';
-// Global app controller
-const APIpath = `https://forkify-api.herokuapp.com/api/search`; // The Api built by Jonas
+import Search from './models/Search';
 
-async function getResults(query) {
-    
-    try {
-        const res = await axios(`${APIpath}?q=${query}`);
-        const recipes = res.data.recipes;
-        
-        console.log(recipes.forEach(el => {
-            console.log(el.title);      
-            console.log(el);      
-            console.log(``);
-        }) );
-        
-    } catch (error) {
-        alert(error)
-    }
+const searchPizza = new Search(`Pizza`);
 
-}
-
-getResults(`pizza`);
+console.log(searchPizza);
+searchPizza.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,29 @@
-export default "I am an default exported string!"
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(){
+
+        try {
+            const APIpath = `https://forkify-api.herokuapp.com/api/search`; // The API built by Jonas
+
+            const result = await axios(`${APIpath}?q=${this.query}`);
+            this.recipes = result.data.recipes; // an array of recipe objects
+
+            /* 
+            console.log(this.recipes.forEach(el => {
+                console.log(el.title);      
+                console.log(el);      
+                console.log(``);
+            }) );
+             */
+            
+        } catch (error) {
+            alert(error)
+        }
+
+    }
+}
