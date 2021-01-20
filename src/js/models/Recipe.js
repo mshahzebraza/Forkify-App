994fcfb68a603a9d2cbe67ea099e37060c982e82
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2021-01-20 22:00:27 +0500

    Added some corrections to Lec 148
    Some minor changes.... Also removed some IF statement with a general code statement

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -71,9 +71,16 @@ export default class Recipe {
             // 3. PARSE INGREDIENTS INTO COUNT AND INGREDIENT 
             if (unitIndex > -1) {
                 // Has Unit , Has Count // True if unit is matched at any place of the sentence
+                // console.log( arrIngredient.slice(0,unitIndex) ); // means everything before the unitIndex is coerced into the 'Count'
+                // console.log( arrIngredient.slice(0,unitIndex).join("+") ); // means everything before the unitIndex is coerced into the 'Count'
+                // console.log( eval( arrIngredient.slice(0,unitIndex).join("+")) ); // means everything before the unitIndex is coerced into the 'Count'
                 objIngredient= {
                     // count: eval( arrIngredient.slice(0,unitIndex+1).join("+") ), // ERROR Here!
-                    count: eval( arrIngredient.slice(0,unitIndex).join("+") ), // means everything before the unitIndex is coerced into the 'Count'
+                    count: eval( arrIngredient.slice(0,unitIndex).join("+").replace("-","+") ), // Important 
+                    // Saved the IF statement for two cases..
+                    // (Case 1. If the ingredient includes '-' sign, then replace)
+                    // (Case 2. If it doesn't then simply join)
+                    // count: eval( arrIngredient.slice(0,unitIndex).replace("-","+") ), // means everything before the unitIndex is coerced into the 'Count'
                     unit: arrIngredient[unitIndex],
                     ingredient: arrIngredient.slice(1).join(" ")
                 }
@@ -103,8 +110,8 @@ export default class Recipe {
 
             }
             
-           return curIngredient; 
-        //    return objIngredient; 
+        //    return curIngredient; 
+           return objIngredient; 
         });
         this.ingredients = newIngredients;        
         // console.log(this.ingredients);
